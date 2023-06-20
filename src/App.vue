77cb86f8e2769dbc6b0d1903737416ed27676576
DuvanArwenLazar <3dtuningduvan@gmail.com>
2023-06-20 16:08:50 -0500

    Navbar (Iphone 14 pro max) Completed

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,7 @@
 </script>
 
 <template>
-  <div>
-    <h1> Hello World </h1>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <indexNavbar msg="Lana App" />
+  <indexNavbar msg="" />
 </template>
 
 <style scoped>
--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,13 +1,49 @@
 <script setup lang="ts">
-
+  
 </script>
 
 <template>
-  <div>
-    <h1> Hello World </h1>
-    <button></button> 
+  <div class="content">
+    <div class="title">
+      <h1 class="lana-title"> Lana </h1>
+    </div>
+    <div class="buttons">
+      <a href="#" class="login">Log in </a><span> or </span>
+      <button class="register-button"> Register </button> 
+    </div>
   </div>
 </template>
 
 <style scoped>
+  .content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    box-shadow: 0 -1px 8px 0 #000000;
+    padding: 7px 0px;
+    height: 43px;
+  }
+
+  .title {
+    margin-left: 19px;
+    color: var(--secondary-color);
+  }
+
+  .buttons {
+    margin-right: 19px;
+  }
+
+  .register-button {
+    width: 80px;
+    height: 27px;
+    border: none;
+    border-radius: 24px;
+    color: var(--alter-color-two);
+  }
+
+  .register-button { background-color: var(--primary-color); }
+  a { text-decoration: none; }
+  a:visited { text-decoration: none; }
+  span, a { color: var(--secondary-color); }
+  span { padding: 0px 4px; }
 </style>
