990a60c450f5dbde7f6836521ef479ec66ab79d5
Kolozuz <moralesjuanpablo1122@gmail.com>
2023-09-10 00:26:03 -0500

    Correct the misspelled "history/ies" to "story/ies", closes #6

---
+++ src/views/view_story.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import component_history from '../components/component_story.vue';
+import { users_info } from "./users_info";
+</script>
+
+<template>
+    <div>
+        <h2 class="share-title">Tell your story!</h2>
+    </div>
+    <div class="stories_container">
+        <component_history v-for="({user_id}, i) in users_info" :id="user_id" :alt="'storie #'+i"/>
+    </div>
+</template>
+
+<style scoped>
+</style>
