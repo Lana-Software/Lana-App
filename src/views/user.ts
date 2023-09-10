9c27acaef3c3797dba2eb6f82bfc4f34b19c3d1b
Kolozuz <moralesjuanpablo1122@gmail.com>
2023-09-10 00:29:42 -0500

    Move code to separate file

---
+++ src/views/user.ts
@@ -0,0 +1,1 @@
+export interface user { user_id: number; user_profile: string; recent_post: { comments: number; likes: number; image: string; }; }
---
+++ src/views/users_info.ts
@@ -0,0 +1,40 @@
+import { user } from "./user";
+
+export let users_info: Array<user> = [
+{
+user_id: 10010210201,
+user_profile: 'profile.jpg',
+recent_post: {
+comments: 18,
+likes: 69,
+image: 'porsche.jpg'
+}
+},
+{
+user_id: 5434343243,
+user_profile: 'profile.jpg',
+recent_post: {
+comments: 54,
+likes: 45,
+image: ''
+}
+},
+{
+user_id: 5434343243,
+user_profile: 'profile.jpg',
+recent_post: {
+comments: 2,
+likes: 180,
+image: 'profile.jpg'
+}
+},
+{
+user_id: 10010210201,
+user_profile: 'profile.jpg',
+recent_post: {
+comments: 19,
+likes: 670,
+image: 'porsche.jpg'
+}
+}
+];
