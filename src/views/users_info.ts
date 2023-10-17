647b999087c25b01a895db2c2552f0523529dca8
Kolozuz <moralesjuanpablo1122@gmail.com>
2023-10-17 02:18:43 -0500

    Make imgs dynamic

--- src/views/users_info.ts
+++ src/views/users_info.ts
@@ -1,30 +1,12 @@
 import { user } from "./user";
+import {getRandomStr, getRandomInt, getRandomColor} from "../../public/ts/utilities";
 
 let imgid = 0
-function getRandomInt(num:number){
-    return Math.floor(Math.random() * num + (num/1.5))
-}
-
-
-function getRandonStr(length){
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-    const characterslength = characters.length;
-    let result = ''
-    let index = 0
-    
-    while (index < length) {
-        result += characters.charAt(Math.floor(Math.random() * characterslength));
-        console.log(result)
-        index+=1
-    }
-
-    return result;
-}
 
 export let users_info: Array<user> = [
     {
         user_id: 100199001,
-        user_profile: 'https://api.dicebear.com/7.x/initials/svg?seed=' + getRandonStr(2),
+        user_profile: 'https://api.dicebear.com/7.x/notionists/svg?scale=150&backgroundType=gradientLinear,solid&backgroundColor=' + getRandomColor() + ',' + getRandomColor() + '&seed=' + getRandomStr(9),
         recent_post: {
             likes: getRandomInt(999),
             comments: getRandomInt(499.5),
@@ -33,7 +15,7 @@ export let users_info: Array<user> = [
     },
     {
         user_id: 100199002,
-        user_profile: 'https://api.dicebear.com/7.x/initials/svg?seed=' + getRandonStr(2),
+        user_profile: 'https://api.dicebear.com/7.x/initials/svg?seed=' + getRandomStr(2),
         recent_post: {
             likes: getRandomInt(999),
             comments: getRandomInt(499.5),
@@ -42,7 +24,7 @@ export let users_info: Array<user> = [
     },
     {
         user_id: 100199003,
-        user_profile: 'https://api.dicebear.com/7.x/initials/svg?seed=' + getRandonStr(2),
+        user_profile: 'https://api.dicebear.com/7.x/notionists/svg?scale=150&backgroundType=gradientLinear,solid&backgroundColor=' + getRandomColor() + ',' + getRandomColor() + '&seed=' + getRandomStr(9),
         recent_post: {
             likes: getRandomInt(999) ,
             comments: getRandomInt(499.5),
@@ -51,7 +33,7 @@ export let users_info: Array<user> = [
     },
     {
         user_id: 100199004,
-        user_profile: 'https://api.dicebear.com/7.x/initials/svg?seed=' + getRandonStr(2),
+        user_profile: 'https://api.dicebear.com/7.x/initials/svg?seed=' + getRandomStr(2),
         recent_post: {
             likes: getRandomInt(999),
             comments: getRandomInt(499.5),
@@ -60,7 +42,7 @@ export let users_info: Array<user> = [
     },
     {
         user_id: 100199005,
-        user_profile: 'https://api.dicebear.com/7.x/initials/svg?seed=' + getRandonStr(2),
+        user_profile: 'https://api.dicebear.com/7.x/notionists/svg?scale=150&backgroundType=gradientLinear,solid&backgroundColor=' + getRandomColor() + ',' + getRandomColor() + '&seed=' + getRandomStr(9),
         recent_post: {
             likes: getRandomInt(999),
             comments: getRandomInt(499.5),
@@ -69,7 +51,7 @@ export let users_info: Array<user> = [
     },
     {
         user_id: 100199006,
-        user_profile: 'https://api.dicebear.com/7.x/initials/svg?seed=' + getRandonStr(2),
+        user_profile: 'https://api.dicebear.com/7.x/initials/svg?seed=' + getRandomStr(2),
         recent_post: {
             likes: getRandomInt(999) ,
             comments: getRandomInt(499.5),
