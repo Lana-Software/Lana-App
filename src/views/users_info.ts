3c3c50d1e19f303a0f3bafbd345d76484ba06236
Kolozuz <moralesjuanpablo1122@gmail.com>
2023-10-17 01:01:13 -0500

    first commit

--- src/views/users_info.ts
+++ src/views/users_info.ts
@@ -5,10 +5,26 @@ function getRandomInt(num:number){
     return Math.floor(Math.random() * num + (num/1.5))
 }
 
+
+function getRandonStr(length){
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    const characterslength = characters.length;
+    let result = ''
+    let index = 0
+    
+    while (index < length) {
+        result += characters.charAt(Math.floor(Math.random() * characterslength));
+        console.log(result)
+        index+=1
+    }
+
+    return result;
+}
+
 export let users_info: Array<user> = [
     {
         user_id: 100199001,
-        user_profile: 'jhonny_pfp.png',
+        user_profile: 'https://api.dicebear.com/7.x/initials/svg?seed=' + getRandonStr(2),
         recent_post: {
             likes: getRandomInt(999),
             comments: getRandomInt(499.5),
@@ -17,7 +33,7 @@ export let users_info: Array<user> = [
     },
     {
         user_id: 100199002,
-        user_profile: 'jojopuppy1.jpg',
+        user_profile: 'https://api.dicebear.com/7.x/initials/svg?seed=' + getRandonStr(2),
         recent_post: {
             likes: getRandomInt(999),
             comments: getRandomInt(499.5),
@@ -26,7 +42,7 @@ export let users_info: Array<user> = [
     },
     {
         user_id: 100199003,
-        user_profile: 'jojopuppy2.png',
+        user_profile: 'https://api.dicebear.com/7.x/initials/svg?seed=' + getRandonStr(2),
         recent_post: {
             likes: getRandomInt(999) ,
             comments: getRandomInt(499.5),
@@ -35,7 +51,7 @@ export let users_info: Array<user> = [
     },
     {
         user_id: 100199004,
-        user_profile: 'jojopuppy3.jpg',
+        user_profile: 'https://api.dicebear.com/7.x/initials/svg?seed=' + getRandonStr(2),
         recent_post: {
             likes: getRandomInt(999),
             comments: getRandomInt(499.5),
@@ -44,7 +60,7 @@ export let users_info: Array<user> = [
     },
     {
         user_id: 100199005,
-        user_profile: 'jojopuppy4.png',
+        user_profile: 'https://api.dicebear.com/7.x/initials/svg?seed=' + getRandonStr(2),
         recent_post: {
             likes: getRandomInt(999),
             comments: getRandomInt(499.5),
@@ -53,7 +69,7 @@ export let users_info: Array<user> = [
     },
     {
         user_id: 100199006,
-        user_profile: 'manzanagato.png',
+        user_profile: 'https://api.dicebear.com/7.x/initials/svg?seed=' + getRandonStr(2),
         recent_post: {
             likes: getRandomInt(999) ,
             comments: getRandomInt(499.5),
