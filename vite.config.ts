321f965804586afc81c2b1aa2c60a8dc876b8322
Kolozuz <moralesjuanpablo1122@gmail.com>
2024-10-02 10:37:53 -0500

    add node packages polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/Lana-App/",
-	plugins: [vue()],
+	base: "/",
+	plugins: [
+		vue(),
+		//Fix mongodb libraries
+		nodePolyfills({
+			include: [
+				'crypto',
+				'stream',
+				'util',
+				'zlib',
+				'os'
+			]
+		})
+	],
 	test: {
 		reporters: "verbose",
 	},
