d2daff08be67a48daa3e9e65d7909211a9b75c38
Kolozuz <moralesjuanpablo1122@gmail.com>
2024-10-02 10:41:58 -0500

    remove not used code

--- src/components/component_footer.vue
+++ src/components/component_footer.vue
@@ -1,16 +1,4 @@
 <script setup lang="ts">
-/* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core'
-
-/* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-/* import specific icons */
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
-
-/* add icons to the library */
-library.add(faGithub)
-
 </script>
 
 <template>
