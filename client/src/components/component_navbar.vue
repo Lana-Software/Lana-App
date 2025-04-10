953ff09c2aaad4a509072748d59548bf05b7f0f9
Kolozuz <moralesjuanpablo1122@gmail.com>
2025-04-09 20:08:52 -0500

    feat: fix misspelling of "characters" and tweak indentation

--- client/src/components/component_navbar.vue
+++ client/src/components/component_navbar.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
-import { useCharatersStore } from '../store/charaters'
+import { useCharactersStore } from '../store/characters'
 import { useRouter } from 'vue-router'
 import { ref } from 'vue'
 
-const charatersStore = useCharatersStore()
+const charactersStore = useCharactersStore()
 
 const router = useRouter()
 
 const query = ref('')
 const submitQuery = (e: Event) => {
-  e.preventDefault()
-  charatersStore.searchCharaters(query.value)
-  router.push({ name: 'Search', params: { query: query.value } })
+	e.preventDefault()
+	charactersStore.searchCharacters(query.value)
+	router.push({ name: 'Search', params: { query: query.value } })
 }
 </script>
 
@@ -46,72 +46,93 @@ const submitQuery = (e: Event) => {
     margin: 0;
   }
 
-  .title {
-    margin-left: 19px;
-    color: var(--secondary-color);
-  }
+.title {
+	margin-left: 19px;
+	color: var(--secondary-color);
+}
 
-	.search{
-		flex: 1 1 content;
-		max-width: 40%;
-	}
+.search {
+	flex: 1 1 content;
+	max-width: 40%;
+}
 
-	.search>form{
-		display: flex;
-		flex: 1 1 content;
-		height: 27px;
-		align-items: center;
-		/* border: solid black 1px; */
-    box-shadow: 0 0 5px 0 #000000;
-		border-radius: 24px;
-		overflow: hidden;
-	}
+.search>form {
+	display: flex;
+	flex: 1 1 content;
+	height: 27px;
+	align-items: center;
+	box-shadow: 0 0px 5px 0 rgba(0, 0, 0, 0.5);
+	border-radius: 24px;
+	overflow: hidden;
+}
 
-  .search-bar{
+.search-bar {
 	flex: 1 1 auto;
-    padding: 5px;
+	padding: 5px;
 	border: none;
-  }
+}
 
-  .search-bar:focus{
-    outline: none;
-  }
+.search-bar:focus {
+	outline: none;
+}
 
-  .search-btn{
+.search-btn {
 	flex: 0 1 auto;
 	width: 35px;
 	height: 100%;
 	border: none;
-  }
+}
 
-  .buttons {
-    margin-right: 19px;
-  }
+.buttons {
+	margin-right: 19px;
+}
 
-  .register-button {
-    width: 80px;
-    height: 27px;
-    border: none;
-    border-radius: 24px;
-    color: var(--alter-color-two);
-    padding: 5px;
-  }
+.register-button {
+	width: 80px;
+	height: 27px;
+	border: none;
+	border-radius: 24px;
+	color: var(--alter-color-two);
+	padding: 5px;
+	background-color: var(--primary-color);
+}
 
-  .register-button:hover {
-    background-color: #614ae6;
-    transition: 0.4s;
-  }
+.register-button:hover {
+	background-color: #614ae6;
+	transition: 0.4s;
+}
 
-  .register-button:not(:hover) {
-    background-color: (--alter-color-two);
-    transition: 0.4s;
-  }
+.register-button:not(:hover) {
+	background-color: (--alter-color-two);
+	transition: 0.4s;
+}
 
-  .register-button { background-color: var(--primary-color); }
-  .link { text-decoration: none; }
-  .link:visited { text-decoration: none; }
-  .link:hover { text-decoration: underline; }
-  span, .link { color: var(--secondary-color); }
-  span { padding: 0px 4px; }
-  .unformatted-link, .unformatted-link:visited{text-decoration: none; color: initial;}
+.link {
+	text-decoration: none;
+}
+
+.link:visited {
+	text-decoration: none;
+}
+
+.link:hover {
+	text-decoration: underline;
+}
+
+span,
+.link {
+	color: var(--secondary-color);
+}
+
+span {
+	padding: 0px 4px;
+}
+
+.unformatted-link,
+.unformatted-link:visited {
+	text-decoration: none;
+	color: initial;
+}
+
+
 </style>
